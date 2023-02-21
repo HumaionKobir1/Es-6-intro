ac2533ffb7324d34b8e030dc21abfb91b3938d54
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-21 16:16:10 +0600

    Template string, multiple line string, dynamic string

---
+++ template.js
@@ -0,0 +1,28 @@
+// const person = 'Adam Sandler';
+// const person2 = "Ben White";
+// const person3 = `Donald Trump`;
+
+const multiLine = 'First Line text \n' +
+'second line of code \n' +
+'third line of text \n' +
+'Fourth line of string';
+console.log(multiLine);
+
+const multiLine1 = `
+Frist Line text
+Second line of code
+third line of text
+Fourth line of string
+`;
+console.log(multiLine1);
+
+
+const a = 20;
+const b = 30;
+const newSummery = `sum of ${a}  and ${b} is: ${a+b}`
+console.log(newSummery);
+
+const numbs = [78, 98, 54, 45, 444];
+
+const Summery = `sum of ${a}  and ${numbs.length} is: ${a+b}`;
+console.log(Summery);
