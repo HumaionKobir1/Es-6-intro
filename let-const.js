cabe80572c13f81d69eeadf640d70e35fbd1bbeb
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-21 15:27:26 +0600

    difference between var, let and const

---
+++ let-const.js
@@ -0,0 +1,24 @@
+let money = 25;
+money = 35;
+console.log(money);
+
+const bird = 'Jaan pakhi';
+const message = bird + ' potas potas';
+console.log(message);
+
+const numbers = [12, 89, 65, 45];
+numbers.push(123);
+numbers[1] = 90;
+console.log(numbers);
+
+
+const student = {
+    name: 'mofiz',
+    address: 'Rangpur'
+};
+student.name = 'Mofazzol';
+
+for(let i = 0; i < numbers.length; i++){
+    const number = numbers[i];
+    console.log(number);
+}
